b0fbf680ac16034aad0d6adf93e08cbf1e13791c
Henri <148531229+HenriMichelon@users.noreply.github.com>
2026-05-11 13:13:44 +0200

    Fix window manager removal logic

--- src/Window.cpp
+++ src/Window.cpp
@@ -24,9 +24,9 @@ namespace lysa::ui {
     }
 
     Window::~Window() {
-        if (windowManager) {
-            static_cast<WindowManager*>(windowManager)->remove(this->shared_from_this());
-        }
+        // if (windowManager) {
+            // static_cast<WindowManager*>(windowManager)->remove(this->shared_from_this());
+        // }
     }
 
     void Window::attach(void* windowManager) {
